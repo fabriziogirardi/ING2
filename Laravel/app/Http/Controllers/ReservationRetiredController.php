9487e7443427c8977eb577681fb45f6da6fcee10
fabro <fabro.la22@gmail.com>
2025-05-19 13:03:46 -0300

    Primera etapa de preparación de contexto para la primera sprint.

---
+++ Laravel/app/Http/Controllers/ReservationRetiredController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReservationRetiredRequest;
+use App\Http\Requests\UpdateReservationRetiredRequest;
+use App\Models\ReservationRetired;
+
+class ReservationRetiredController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreReservationRetiredRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ReservationRetired $reservationRetired)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ReservationRetired $reservationRetired)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateReservationRetiredRequest $request, ReservationRetired $reservationRetired)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ReservationRetired $reservationRetired)
+    {
+        //
+    }
+}
