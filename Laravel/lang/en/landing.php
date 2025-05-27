2aba63331345abb735b31cfafd02d5ca4170641c
neftalito <47830827+neftalito@users.noreply.github.com>
2025-05-27 23:05:24 +0000

    Añadir Localizacion

---
+++ Laravel/lang/en/landing.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'title'       => 'Alquil.ar - You need it, we have it.',
+    'login'       => 'Login',
+    'branches'    => 'Branches',
+    'viewCatalog' => 'View Catalog',
+];
---
+++ Laravel/lang/es/landing.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'title'       => 'Alquil.ar - Lo necesitas, lo tenemos.',
+    'login'       => 'Iniciar Sesión',
+    'branches'    => 'Sucursales',
+    'viewCatalog' => 'Ver Catálogo',
+    'contactInfo' => 'Información de contacto',
+];
