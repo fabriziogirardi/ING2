03df185fcd9b72afcd603f997ac6497f7f58398c
Neftalí Toledo <47830827+neftalito@users.noreply.github.com>
2025-06-01 23:25:41 -0300

    ING2-41 CRUD Sucursales (#44)

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Customer\Auth\LoginController as CustomerLoginControlle
 use App\Http\Controllers\Employee\Auth\LoginController as EmployeeLoginController;
 use App\Http\Controllers\Manager\Auth\LoginController as ManagerLoginController;
 use App\Http\Controllers\Manager\Auth\VerifyTokenController;
-use App\Http\Controllers\Manager\Branches\BranchesListing;
+use App\Http\Controllers\Manager\Branches\BranchController;
 use App\Http\Controllers\Manager\Brand\BrandController;
 use App\Http\Controllers\Manager\Model\ModelController;
 use App\Models\Category;
@@ -82,7 +82,7 @@ Route::group(['prefix' => 'manager', 'as' => 'manager.'], static function () {
 
         Route::resource('brand', BrandController::class);
 
-        Route::get('/viewBranches', [BranchesListing::class, '__invoke'])->name('branches.index');
+        Route::resource('branch', BranchController::class);
     });
 });
 // endregion
