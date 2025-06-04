dc19ddda8c619dfc45afd67ec4d52cd7253878dd
neftalito <47830827+neftalito@users.noreply.github.com>
2025-06-04 18:07:15 +0000

    ING2-42 Front alta de productos

--- Laravel/lang/es/manager/product.php
+++ Laravel/lang/es/manager/product.php
@@ -4,4 +4,6 @@ return [
     'created' => 'Maquinaria creada exitosamente.',
     'updated' => 'Maquinaria modificada exitosamente.',
     'deleted' => 'Maquinaria borrada exitosamente',
+    'addProduct' => 'Añadir nueva maquinaria',
+    'addProductFormTitle' => 'Añadir nueva maquinaria'
 ];
