18bb8146535eb94dec6c6a2e8e7d7b30a5f53caf
fabro <fabro.la22@gmail.com>
2025-06-05 16:40:38 -0300

    Componentes de formularios, navbar triple, links y errores. Funcionalidades de login para todos.

--- Laravel/resources/views/components/navigation/accounts/employee-menu.blade.php
+++ Laravel/resources/views/components/navigation/accounts/employee-menu.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-col md:flex-row flex-grow-0">
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
     <x-elements.link-button href="{{ route('employee.logout') }}" text="{{ __('employee/auth.logout_button') }}"
-                            icon-left="fa-solid fa-right-from-bracket" type="danger" />
-    <x-navigation.navbar.account-badge :person="auth()->guard('employee')->user()->person" user-type="{{ __('auth.role_employee') }}" />
+                            icon-left="fa-solid fa-right-from-bracket" type="danger" class="order-1 lg:order-2" />
+    <x-navigation.navbar.account-badge :person="auth()->guard('employee')->user()->person" user-type="{{ __('auth.role_employee') }}" class="order-2 lg:order-1" />
 </div>
