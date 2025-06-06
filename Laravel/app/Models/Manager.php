143ec78020c41e72250464afbdf413f48dd6fd21
fabro <fabro.la22@gmail.com>
2025-06-06 11:28:31 -0300

    Eliminar la columna "chota" que era una prueba y se commiteo sin querer. Acomodar relaciones de Empleado, Gerente y Persona

--- Laravel/app/Models/Manager.php
+++ Laravel/app/Models/Manager.php
@@ -19,8 +19,6 @@ class Manager extends Authenticatable
     /** @use HasFactory<\Database\Factories\ManagerFactory> */
     use HasFactory;
 
-    protected $with = ['person'];
-
     protected $fillable = [
         'person_id',
         'password',
