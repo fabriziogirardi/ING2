b96ba45380c8fe39fe9f5624e9a22772b5dd02a6
fabro <fabro.la22@gmail.com>
2025-06-06 15:31:07 -0300

    Fix de botones

--- Laravel/resources/views/components/elements/link-button.blade.php
+++ Laravel/resources/views/components/elements/link-button.blade.php
@@ -1,15 +1,15 @@
 <a href="{{ !$disabled ? $href : "#" }}" {{ $attributes->merge(['class' => $$type . ($disabled ? ' opacity-25 cursor-not-allowed' : '') . ($fullWidth ? ' w-full' : '')]) }} target="{{ $target }}">
     <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-transparent w-full">
         @if($iconLeft)
-            <span class="inline-block mr-2">
+            <span class="inline-block">
                 <i class="{{ $iconLeft }}"></i>
             </span>
         @endif
         @if($text)
-            {{ $text }}
+                <span class="mx-2">{{ $text }}</span>
         @endif
         @if($iconRight)
-            <span class="inline-block ml-2">
+            <span class="inline-block">
                 <i class="{{ $iconRight }}"></i>
             </span>
         @endif
