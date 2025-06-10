2bec2bb3b6dcfe12263973db348af0c6b761b420
Fabrizio Girardi <fabro.la22@gmail.com>
2025-06-10 18:25:16 -0300

    Tabla de reservas

--- Laravel/app/Models/Reservation.php
+++ Laravel/app/Models/Reservation.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 /**
@@ -13,6 +16,31 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
  */
 class Reservation extends Model
 {
+    use HasFactory, SoftDeletes;
+    
     /** @use HasFactory<\Database\Factories\ReservationFactory> */
-    use HasFactory;
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'code',
+        'start',
+        'end',
+    ];
+    
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+    
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+    
+    public function duration(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->start->diffInDays($this->end),
+        );
+    }
 }
--- Laravel/database/migrations/2025_05_19_110704_create_reservations_table.php
+++ Laravel/database/migrations/2025_05_19_110704_create_reservations_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Customer;
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,9 +13,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reservations', function (Blueprint $table) {
+        Schema::create('reservations', static function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Customer::class);
+            $table->foreignIdFor(Product::class);
+            $table->char('code', 8)->unique();
+            $table->date('start');
+            $table->date('end');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
