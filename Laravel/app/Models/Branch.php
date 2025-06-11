de555498fa75100d74da4991828ce3663505b2a3
neftalito <47830827+neftalito@users.noreply.github.com>
2025-06-11 14:35:28 +0000

    Corregir SoftDeletes

--- Laravel/app/Models/Branch.php
+++ Laravel/app/Models/Branch.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 /**
@@ -16,7 +17,7 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 class Branch extends Model
 {
     /** @use HasFactory<\Database\Factories\BranchFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'place_id',
