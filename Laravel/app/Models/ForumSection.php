415bb7347edc6790129f29450b74a388bc0993c9
UnMatesito <mateoelcool986@gmail.com>
2025-06-30 20:13:32 +0000

    Agrego Restriccion de eliminacion de secciones si se tiene discusiones asociadas

--- Laravel/app/Models/ForumSection.php
+++ Laravel/app/Models/ForumSection.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 /**
@@ -20,4 +21,20 @@ class ForumSection extends Model
     ];
 
     protected $table = 'forum_sections';
+
+    public function discussions(): HasMany
+    {
+        return $this->hasMany(ForumDiscussion::class, 'forum_section_id');
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($section) {
+            if ($section->discussions()->exists()) {
+                return false; // Cancels the delete operation
+            }
+        });
+    }
 }
