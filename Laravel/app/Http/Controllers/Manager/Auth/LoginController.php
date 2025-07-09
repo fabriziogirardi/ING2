1711ae1e43c4b6ec7371fe49e9cd6355687b4eaf
imnotsetro <matias.abreg@gmail.com>
2025-07-09 11:19:59 -0300

    ING2-147 Mensajes de error específicos para los inicios de sesión

--- Laravel/app/Http/Controllers/Manager/Auth/LoginController.php
+++ Laravel/app/Http/Controllers/Manager/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Manager\Auth\LoginRequest;
 use App\Http\Requests\Manager\Auth\TokenRequest;
 use App\Mail\Manager\TokenGeneratedMail;
 use App\Models\Manager;
+use App\Models\Person;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -26,11 +27,32 @@ class LoginController extends Controller
      */
     public function loginAttempt(LoginRequest $request): RedirectResponse
     {
+        $credentials = [
+            'email' => $request->validated('email'),
+            'password' => $request->validated('password'),
+        ];
+
+        $manager = Person::where('email', $credentials['email'])->first();
+
+        if (! $manager) {
+            return redirect()->back()->withErrors([
+                'credentials' => 'No existe una persona registrada con ese email en el sistema',
+            ]);
+        }
+
+        $manager = Manager::where('person_id', $manager->id)->first();
+
+        if (! $manager) {
+            return redirect()->back()->withErrors([
+                'credentials' => 'La persona registrada con ese email no tiene una cuenta de manager',
+            ]);
+        }
+
         $manager = Manager::whereRelation('person', 'email', $request->validated('email'))->first();
 
         if (! $manager || ! password_verify($request->validated('password'), $manager->password)) {
             return redirect()->back()->withErrors([
-                'credentials' => __('manager/auth.incorrect_credentials'),
+                'credentials' => 'Contraseña incorrecta',
             ]);
         }
 
