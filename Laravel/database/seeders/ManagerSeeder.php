8ba95fd2fd5f722bea300c08e0a50d71eb0bd448
Fabrizio Girardi <fabro.la22@gmail.com>
2025-07-11 02:16:48 -0300

    Fixes para prod en VPS

--- Laravel/database/seeders/ManagerSeeder.php
+++ Laravel/database/seeders/ManagerSeeder.php
@@ -15,18 +15,48 @@ class ManagerSeeder extends Seeder
      */
     public function run(): void
     {
-        $person = Person::create([
+        Person::create([
             'first_name'            => 'Mario',
             'last_name'             => 'Bro',
             'email'                 => 'fabro.la22@gmail.com',
             'birth_date'            => now()->subYears(30)->format('Y-m-d'),
             'government_id_type_id' => GovernmentIdType::where('name', 'DNI')->first()->id,
             'government_id_number'  => '12345678',
-        ]);
-
-        Manager::create([
-            'person_id' => $person->id,
+        ])->manager()->create([
             'password'  => Hash::make('aguanteluigi'),
+        ]);
+        
+        Person::create([
+            'first_name'            => 'Mario',
+            'last_name'             => 'Bro',
+            'email'                 => 'matias.abrego@hotmail.com',
+            'birth_date'            => now()->subYears(30)->format('Y-m-d'),
+            'government_id_type_id' => GovernmentIdType::where('name', 'DNI')->first()->id,
+            'government_id_number'  => '22345678',
+        ])->manager()->create([
+            'password'  => Hash::make('aguanteluigi'),
+        ]);
+        
+        Person::create([
+            'first_name'            => 'Mario',
+            'last_name'             => 'Bro',
+            'email'                 => 'mateosuarez1905@hotmail.com',
+            'birth_date'            => now()->subYears(30)->format('Y-m-d'),
+            'government_id_type_id' => GovernmentIdType::where('name', 'DNI')->first()->id,
+            'government_id_number'  => '32345678',
+        ])->manager()->create([
+            'password' => Hash::make('aguanteluigi'),
+        ]);
+        
+        Person::create([
+            'first_name'            => 'Mario',
+            'last_name'             => 'Bro',
+            'email'                 => 'neftalitosu@gmail.com',
+            'birth_date'            => now()->subYears(30)->format('Y-m-d'),
+            'government_id_type_id' => GovernmentIdType::where('name', 'DNI')->first()->id,
+            'government_id_number'  => '32345678',
+        ])->manager()->create([
+            'password' => Hash::make('aguanteluigi'),
         ]);
     }
 }
